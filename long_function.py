bd60cbfa8c6469ba406f448249ff1bfbb1167eb6
Alan Marquina <74053975+amarquinar@users.noreply.github.com>
2022-10-18 21:25:13 +0200

    Create long_function.py

---
+++ long_function.py
@@ -0,0 +1,28 @@
+def very_long_function(long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+    
+    pass
+
+def very_long_function2(long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+    
+    pass
+
+def very_long_function3(long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+    
+    pass
+
+def very_long_function4(long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+    
+    pass
+
+
+class my_class:
+    def __init__(self):
+        pass
+
+    def very_long_function_class1(self, long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+    
+        pass
+
+    def very_long_function_class2(self, long_variable_name, long_variable_name2, long_variable_name3, long_variable_name4, long_variable_name5, long_variable_name6, long_variable_name7):
+        
+        pass
